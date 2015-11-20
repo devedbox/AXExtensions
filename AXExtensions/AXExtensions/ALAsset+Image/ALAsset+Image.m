4a1f13876506553ac18e3aef1127b7d2d2654c69
aiXing <862099730@qq.com>
2015-11-20 17:12:42 +0800

    ignored warnings

--- AXExtensions/AXExtensions/ALAsset+Image/ALAsset+Image.m
+++ AXExtensions/AXExtensions/ALAsset+Image/ALAsset+Image.m
@@ -7,9 +7,11 @@
 //
 
 #import "ALAsset+Image.h"
-
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated"
 @implementation ALAsset (Image)
 - (UIImage *)image {
     return [UIImage imageWithCGImage:[[self defaultRepresentation] fullResolutionImage]];
 }
 @end
+#pragma clang diagnostic pop
